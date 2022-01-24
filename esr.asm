38e8d810229e3d898b2a9a83ea68bf023c443d1f
DarwinNE <davbucci@gmail.com>
2022-01-24 23:02:40 +0100

    Added 2022 to the year...

--- esr.asm
+++ esr.asm
@@ -8,7 +8,7 @@
 ; License:
 ; --------
 
-;    Copyright (C) 2021  Davide Bucci  davbucciPleaseNoSpamHerE@tiscali.it
+;    Copyright (C) 2021-2022 Davide Bucci  davbucciPleaseNoSpamHerE@tiscali.it
 
 ;    This program is free software; you can redistribute it and/or modify
 ;    it under the terms of the GNU General Public License as published by
